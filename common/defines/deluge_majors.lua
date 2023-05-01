cd36af9c9785696b67d7e5992c18bc347f3f9148
CommGesen <CommGesen@gmail.com>
2023-05-01 13:35:36 +0300

    Initial commit

---
+++ common/defines/deluge_majors.lua
@@ -0,0 +1,2 @@
+NDefines.NCountry.MAJOR_MIN_FACTORIES = 16						-- need at least these many factories to become a major
+NDefines.NCountry.ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO = 0.3		-- Countries will also be considered major when having more factories that the average of top MIN_MAJOR_COUNTRIES countries' factories times ADDITIONAL_MAJOR_COUNTRIES_IC_RATIO
